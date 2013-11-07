8c48706b635cb191994d5fc989de16edf56915e3
Tan Li Hau <tanl0135@e.ntu.edu.sg>
2013-11-07 15:00:28 +0800

    Note for DeSheng

--- 2002 Assignment/src/storage/UserStorageHandler.java
+++ 2002 Assignment/src/storage/UserStorageHandler.java
@@ -3,6 +3,9 @@ package storage;
 import java.io.File;
 import java.util.ArrayList;
 
+import model.customer.Customer;
+import model.customer.SeniorCustomer;
+import model.customer.StudentCustomer;
 import model.customer.User;
 
 public class UserStorageHandler extends StorageHandler{
@@ -29,11 +32,48 @@ public class UserStorageHandler extends StorageHandler{
 	@Override
 	protected void interpretLine(String line) {
 		//TODO interpret a line and convert into user object and store it into user list
+		/*
+		 
+		User user;
+		Customer c = user.getCustomer();
+		
+		- check whether the customer is a senior or a student or normal
+		c instanceof SeniorCustomer
+		c instanceof StudentCustomer
+		
+		- check whether the user is admin
+		user.isAdmin()
+		
+		- to create admin
+		User.createAdmin(name, password, mobileNumber, emailAddress)
+		
+		- to create user
+		User.createUser(name, password, mobileNumber, age, emailAddress)
+		
+		- add into userlist
+		userList.add(user);
+		*/
 	}
 
 	@Override
 	protected String getDataToWrite() {
 		// TODO translate user list into string which can be interpreted by interpretLine
+		/*
+		
+		- iterate the user list
+		for (User user : userList) {
+			
+		}
+		- get customer from user
+		User user;
+		Customer customer = user.getCustomer();
+		
+		- customer information
+		customer.getAge();
+		customer.getEmailAddress();
+		customer.getMobileNumber();
+		customer.getName();
+		*/
 		return null;
 	}
 
